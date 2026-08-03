fef11ce4e9d221d056b755ece9dca9f31bee5c8b
divyalsurse <divyal.real14@gmail.com>
2026-08-04 02:10:35 +0530

    set up REST API with user CRUD routes, MongoDB connection, and request logger middleware

---
+++ Project-01/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const fs = require("fs");
+
+const {connectMongoDb} = require('./connection')
+const userRouter = require('./routes/user')
+const {logReqRes} = require("./middlewares")
+
+
+const app = express();
+const PORT = 8000;
+
+//connection
+connectMongoDb("mongodb://127.0.0.1:27017/yt-app-1").then(()=>
+  console.log("MongoDB Connected!")
+)
+
+//middleware -plugin
+app.use(express.urlencoded({ extended: false }));
+app.use(logReqRes('log.txt'))
+
+//Routes
+app.use("/api/users",userRouter)
+
+app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
---
+++ Project-01/routes/user.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  handleGetAllUser,
+  handleGetUserById,
+  handleUpdateUserById,
+  handleDeleteUserById,
+  handleCreateNewUser,
+} = require("../controllers/user");
+
+const router = express.Router();
+
+router.route("/").get(handleGetAllUser).post(handleCreateNewUser);
+
+router
+  .route("/:id")
+  .get(handleGetUserById)
+  .patch(handleUpdateUserById)
+  .delete(handleDeleteUserById);
+
+module.exports = router;
